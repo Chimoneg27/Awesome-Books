802fd8c483ff46fe126bf7610084d89341d4ccfb
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-08-08 15:29:59 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
+/* eslint-disable new-cap */
 const bookShelf = [];
 const bookForm = document.getElementById('book-entry');
 const bookStore = document.getElementById('bookStore');
-const bookName = document.querySelector('.book-title');
-const bookAuthor = document.querySelector('books-author');
-const btn = document.getElementById('new-book'); 
 
 function bookObj(name, author) {
-    this.name = name;
-    this.author = author;
+  this.name = name;
+  this.author = author;
 }
 
 function generateBooks() {
-    bookStore.innerHTML = '';
-    for(let i = 0; i < bookShelf.length; i++) {
-        const divElement = document.createElement('div');
-        divElement.className = 'reader';
-        const book = bookShelf[i];
-        divElement.innerHTML = `
+  bookStore.innerHTML = '';
+  for (let i = 0; i < bookShelf.length; i += 1) {
+    const divElement = document.createElement('div');
+    divElement.className = 'reader';
+    const book = bookShelf[i];
+    divElement.innerHTML = `
             <p class='book-title'>${book.name}</p>
             <p class='books-author'>${book.author}</p>
             <button onclick='removeBook(${i})'>Remove</button>
         `;
-        bookStore.appendChild(divElement);
-    }
+    bookStore.appendChild(divElement);
+  }
 }
 
 function addBook() {
-    let title = document.querySelector('#title').value;
-    let author = document.querySelector('#Author').value;
-    let book = new bookObj(title, author);
-    bookShelf.push(book);
-    generateBooks();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#Author').value;
+  const book = new bookObj(title, author);
+  bookShelf.push(book);
+  generateBooks();
 }
+
 function removeBook(index) {
-    bookShelf.splice(index, 1);
-    generateBooks();
+  bookShelf.splice(index, 1);
+  generateBooks();
 }
+
+bookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addBook();
+});
+
+addBook();
+removeBook();
