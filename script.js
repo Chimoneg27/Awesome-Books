badb91d321b1e9b3e84df5bb78df1bb11930fd03
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-08 11:00:49 +0000

    add book variables for the library

---
+++ script.js
@@ -0,0 +1,6 @@
+const bookShelf = [];
+const bookForm = document.getElementById('book-entry');
+const bookStore = document.getElementById('bookStore');
+const bookName = document.querySelector('.book-title');
+const bookAuthor = document.querySelector('books-author');
+const btn = document.getElementById('new-book'); 
