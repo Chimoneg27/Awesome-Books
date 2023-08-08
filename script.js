35595f8deb246045af0bac0fdf03e0edbe644f26
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2023-08-08 13:02:24 +0200

    Add object constructor and generatebooks functions

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const bookStore = document.getElementById('bookStore');
 const bookName = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('books-author');
 const btn = document.getElementById('new-book'); 
+
+function bookObj(name, author) {
+    this.name = name;
+    this.author = author;
+}
+
+function generateBooks() {
+    bookStore.innerHTML = '';
+    for(let i = 0; i < bookShelf.length; i++) {
+        const divElement = document.createElement('div');
+        divElement.className = 'reader';
+        const book = bookShelf[i];
+        divElement.innerHTML = `
+            <p class='book-title'>${book.name}</p>
+            <p class='books-author'>${book.author}</p>
+            <button onclick='removeBook(${i})'>Remove</button>
+        `;
+        bookStore.appendChild(divElement);
+    }
+}
